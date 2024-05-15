4f01ac797c0e1867bad71e01f81ed3958795e5e8
BORIS WOMAGNO <edemwomagno@gmail.com>
2024-05-15 11:20:26 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="../BlogADN/assets/css/style.css">
+ <title>Document</title>
+</head>
+<body>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magni expedita recusandae obcaecati ducimus harum debitis, deleniti sint optio neque, mollitia fuga possimus vitae quibusdam quia, velit animi modi dicta?</p>
+</body>
+</html>
